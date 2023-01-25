6d0ac1c411b6eecc40326d5b7dac3d5ad5822e9c
Morgan <mf1231@york.ac.uk>
2023-01-25 10:49:38 +0000

    First Merge

---
+++ core/src/com/github/brokendesigners/MergeBin/Dispenser.java
@@ -0,0 +1,19 @@
+package com.github.brokendesigners.MergeBin;
+
+//import com.github.brokendesigners.NewStuffs.SpecialItem;
+//
+//public class Dispenser{
+//    private String Ingredient;
+//
+//    public Dispenser(String T)
+//    {
+//        // this.Ingredient = new Item(T);
+//        this.Ingredient = T;
+//    }
+//
+//    public SpecialItem Interact() //When you go up to the station, an ingredient of this type is dropped.
+//    {
+//        return new SpecialItem(this.Ingredient);
+//    }
+//
+//}
---
+++ core/src/com/github/brokendesigners/MergeBin/SpecialItem.java
@@ -0,0 +1,29 @@
+//package com.github.brokendesigners.NewStuffs;
+//
+//public class SpecialItem {
+//    private String name;
+//    public Boolean Cut;
+//    public Boolean Cooking;
+//    public Boolean Cooked;
+//    public Boolean Waste;
+//
+//    public SpecialItem(String n)
+//    {
+//        this.name = n;
+//        this.Cut = false;
+//        this.Cooking = false;
+//        this.Cooked = false;
+//        this.Waste = false;
+//    }
+//
+//    //getname
+//    public String getName()
+//    {
+//        return this.name;
+//    }
+//    public void setName(String x)
+//    {
+//        this.name = x;
+//    }
+//
+//}
