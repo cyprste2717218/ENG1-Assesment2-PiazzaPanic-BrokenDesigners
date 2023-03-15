087eed17f08b9020a288b0055ba7dc3ff42352a6
conncon99 <conncon@yahoo.co.uk>
2023-03-15 10:03:49 +0000

    added testing for customers and customer managers - TODO: Test spawning of customers and check the spawning timer works - this requires a proper test CustomerStation to be implemented first

---
+++ tests/src/de/tomgrill/gdxtesting/unit_tests/character/CustomerManagerTests.java
@@ -0,0 +1,73 @@
+package de.tomgrill.gdxtesting.unit_tests.character;
+
+import com.badlogic.gdx.math.Vector2;
+import com.github.brokendesigners.Match;
+import com.github.brokendesigners.character.CustomerManager;
+import com.github.brokendesigners.enums.GameMode;
+import de.tomgrill.gdxtesting.GdxTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(GdxTestRunner.class)
+public class CustomerManagerTests {
+
+    //Test customer spawning - need proper station testing implementation first
+    //Test timer - need proper station testing implementation first
+
+    @Test
+    public void testGetMeal(){
+        CustomerManager customerManager = createCustomerManager();
+        for(int i = 0; i < 1000; i++){
+            String foodName = customerManager.getMeal();
+            assertTrue(foodName == "Salad" || foodName == "Burger" || foodName == "VoidItem");
+        }
+    }
+
+    @Test
+    public void testIsComplete(){
+        CustomerManager customerManager = createCustomerManager();
+        Match match = customerManager.getMatch();
+        //Scenario
+        match.setGameMode(GameMode.SCENARIO);
+        for(int i = 0; i < customerManager.getCustomerNumber(); i++){
+            assertFalse(customerManager.isComplete());
+            match.incrementCustomersSoFar();
+            System.out.println(match.getCustomersSoFar());
+        }
+        assertTrue(customerManager.isComplete());
+        match.setReputationPoints(-5);
+        assertTrue(customerManager.isComplete());
+        match.setReputationPoints(5);
+        assertTrue(customerManager.isComplete());
+
+        //Endless
+        match.setGameMode(GameMode.ENDLESS);
+        match.setReputationPoints(5);
+        assertFalse(customerManager.isComplete());
+        match.setReputationPoints(-5);
+        assertTrue(customerManager.isComplete());
+    }
+
+    @Test
+    public void testTimeToString(){
+        CustomerManager customerManager = createCustomerManager();
+        assertEquals(customerManager.timeToString(0),"0:00");
+        assertEquals(customerManager.timeToString(60),"1:00");
+        assertEquals(customerManager.timeToString(30),"0:30");
+        assertEquals(customerManager.timeToString(180),"3:00");
+        assertEquals(customerManager.timeToString(194),"3:14");
+        assertEquals(customerManager.timeToString(2),"0:02");
+        assertEquals(customerManager.timeToString(3601),"60:01");
+        assertEquals(customerManager.timeToString(-1),"");
+    }
+
+
+
+    private CustomerManager createCustomerManager(){
+        return new CustomerManager(5, new Vector2(0,0), new Match(GameMode.SCENARIO));
+    }
+
+
+}
---
+++ tests/src/de/tomgrill/gdxtesting/unit_tests/character/CustomerTests.java
@@ -0,0 +1,108 @@
+package de.tomgrill.gdxtesting.unit_tests.character;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.github.brokendesigners.Match;
+import com.github.brokendesigners.character.Customer;
+import com.github.brokendesigners.enums.CustomerPhase;
+import com.github.brokendesigners.enums.GameMode;
+import com.github.brokendesigners.item.Item;
+import com.github.brokendesigners.item.ItemRegister;
+import com.github.brokendesigners.map.interactable.CustomerStation;
+
+import de.tomgrill.gdxtesting.GdxTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+import java.util.Objects;
+
+import static org.junit.Assert.*;
+
+@RunWith(GdxTestRunner.class)
+public class CustomerTests {
+
+
+    //Check customer constructors work properly
+    @Test
+    public void testCustomerSpawn(){
+        Customer customer = createTestCustomer();
+        assertTrue(customer.spawn());
+        assertTrue(customer.isVisible());
+        assertEquals(customer.getPhase(), CustomerPhase.MOVING_TO_STATION);
+    }
+
+    @Test
+    public void testMovingToStationPhase(){
+        Customer customer = createTestCustomer();
+        customer.spawn();
+        customer.setPhase(CustomerPhase.MOVING_TO_STATION);
+        while(customer.getWorldPosition().x != 10 || customer.getWorldPosition().y != 5){
+            assertFalse(Objects.equals(customer.getWorldPosition(), new Vector2(10, 5)));
+            customer.update();
+        }
+        assertTrue(customer.getWorldPosition().x == 10);
+        assertTrue(customer.getWorldPosition().y == 5);
+        customer.update();
+        assertEquals(customer.getPhase(), CustomerPhase.WAITING);
+    }
+
+    @Test
+    public void testWaitingPhase(){
+        //Tests for item correctness will be done in the ServingStationTests class
+        Customer customer = createTestCustomer();
+        customer.spawn();
+        customer.setPhase(CustomerPhase.WAITING);
+        assertEquals(customer.waitingStartTime, -1L);
+        customer.update();
+        assertEquals(customer.waitingStartTime, TimeUtils.millis(), 10L);
+        assertFalse(customer.getPhase() == CustomerPhase.LEAVING);
+        customer.waitingStartTime = customer.customerWaitTime;
+        customer.update();
+        assertEquals(customer.getPhase(), CustomerPhase.LEAVING);
+    }
+
+    @Test
+    public void testLeavingPhase(){
+        Customer customer = createTestCustomer();
+        customer.spawn();
+        customer.setPhase(CustomerPhase.LEAVING);
+        customer.worldPosition = new Vector2(10,5);
+        while(customer.getWorldPosition().x != 2 || customer.getWorldPosition().y != 0){
+            assertFalse(Objects.equals(customer.getWorldPosition(), new Vector2(2, 0)));
+            customer.update();
+        }
+        assertTrue(customer.getWorldPosition().x == 2);
+        assertTrue(customer.getWorldPosition().y == 0);
+        customer.update();
+        assertTrue(customer.getMatch().getCustomersSoFar() == 1);
+        assertEquals(customer.getPhase(), CustomerPhase.DESPAWNING);
+    }
+
+    @Test
+    public void testDespawnPhase(){
+        Customer customer = createTestCustomer();
+        customer.spawn();
+        customer.setPhase(CustomerPhase.DESPAWNING);
+        customer.update();
+        assertFalse(customer.isVisible());
+    }
+
+    private Customer createTestCustomer(Item item){
+        CustomerStation customerStation = Mockito.mock(CustomerStation.class);
+        Mockito.when(customerStation.getCustomerPosition()).thenReturn(new Vector2(10,5));
+        Mockito.when(customerStation.getItemInHand()).thenReturn(item);
+        Mockito.when(customerStation.hasEmptyHand()).thenReturn(item == null);
+        return new Customer(customerStation, item, new Vector2(2,1), new Match(GameMode.SCENARIO));
+    }
+
+    private Customer createTestCustomer(){
+        return createTestCustomer(ItemRegister.itemRegister.get("Burger"));
+    }
+
+
+    //Work on testing different customer phases
+
+    //Can we test spawn function?
+
+}
